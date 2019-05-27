42fdad5075fcb6eec8bc8e9c090b74fb6a245eb7
DESKTOP-PRCLPLD\47613 <CharlesFeng207@gmail.com>
2019-05-27 16:19:50 +0800

    asd

---
+++ hellowworld.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    vector<int> v;
+    cout << v.empty() << endl;
+    v.push_back(99);
+    cout << v.empty() << endl;
+    cout << v[0] << endl;
+    cout << v.size() << endl;
+
+    int a = 1;
+    cout << a << endl;
+    cout << "Hello,World!" << endl;
+    cout << "Hello,World!2" << endl;
+    cout << "123" << endl;
+
+    system("pause");
+    return 1;
+}
