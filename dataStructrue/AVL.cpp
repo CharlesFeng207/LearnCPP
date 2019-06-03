ddcdc31d89fc2d6861c841572d65182a24b4c34b
DESKTOP-PRCLPLD\47613 <CharlesFeng207@gmail.com>
2019-06-03 18:15:19 +0800

    asd

---
+++ dataStructrue/AVL.cpp
@@ -0,0 +1,26 @@
+typedef char KeyType; //关键字
+
+typedef struct MyRcdType //记录
+{
+    KeyType key;
+    
+} RcdType, *RcdArr;
+
+typedef enum MyBFStatus //为了方便平衡因子的赋值，这里进行枚举
+{ //RH,EH,LH分别表示右子树较高，左右子树等高，左子树较高
+
+    RH,
+    EH,
+    LH
+
+} BFStatus;
+
+typedef struct MyBBSTNode //树结点类型定义
+{
+    RcdType data; //数据成员
+
+    BFStatus bf; //平衡因子
+
+    struct MyBBSTNode *lchild, *rchild; //左右分支
+
+} BBSTNode, *BBSTree;
