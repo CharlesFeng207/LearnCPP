cd0d3b32dd7fc4fa9e4b485b10692853857dcf05
DESKTOP-PRCLPLD\47613 <CharlesFeng207@gmail.com>
2019-06-17 13:24:30 +0800

    z

--- leetcode/5.longestPalindromicSubstring.cpp
+++ leetcode/5.longestPalindromicSubstring.cpp
@@ -6,16 +6,37 @@
 
 using namespace std;
 
+string to_manache_string(string &s, string c)
+{
+    string result = c;
+
+    for (auto &sc : s)
+        result += sc + c;
+
+    return result;
+}
+
 string longestPalindrome(string s)
 {
+    if (s.size() == 0)
+        return 0;
+
+    auto manacheStr = to_manache_string(s, "#");
+    vector<int> radius(s.size(), 0);
     
-    return "";
+    int R, C, p1;
+
+    for (int i = 0; i < s.size(); i++)
+    {
+    }
+
+    return manacheStr;
 }
 
 int main()
 {
-
     cout << longestPalindrome("babad") << endl;
+    cout << longestPalindrome("xaaababaaahs") << endl;
     getchar();
     return 0;
 }
