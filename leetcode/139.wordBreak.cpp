d4f5a8544ab995039ad19f4c2511fcd31a01adde
DESKTOP-PRCLPLD\47613 <CharlesFeng207@gmail.com>
2019-06-20 15:02:30 +0800

    z

--- leetcode/139.wordBreak.cpp
+++ leetcode/139.wordBreak.cpp
@@ -5,39 +5,39 @@
 #include <limits.h>
 #include <map>
 #include <string>
-#include <unordered_map>
+#include <unordered_set>
 #include <vector>
 
 using namespace std;
 
-// O(n)
 bool wordBreak(string s, vector<string> &wordDict)
 {
-    vector<int> wordIndex(wordDict.size(), 0);
+    if (wordDict.size() == 0)
+        return false;
+
+    unordered_set<string> dict(wordDict.begin(), wordDict.end());
+
+    // dp[i] represent substr [0 -> i] is valid, so the answer is dp[n-1]
+    // dp[i] = any dp[j] (j < i) exist, let substr [j + 1 -> i] is valid.
+    vector<bool> dp(s.size(), false);
 
     for (int i = 0; i < s.size(); i++)
     {
-        char c = s[i];
-        bool b = true;
-        for (int j = 0; j < wordDict.size(); j++)
+        for (int j = i - 1; j >= -1; j--) // reverse loop of j is more efficient.
         {
-            string &w = wordDict[j];
-            int &wi = wordIndex[j];
-            
-            if(c == w[wi])
+            if (j == -1 || dp[j]) // j == -1 means the whole substr
             {
-                b = false;
-                wi = (wi + 1) % w.size();
+                string substr = s.substr(j + 1, i - j); // len = i - (j + 1) + 1
+                if (dict.find(substr) != dict.end())
+                {
+                    dp[i] = true;
+                    break;
+                }
             }
         }
-
-        if (b)
-            return false;
     }
 
-    bool result = all_of(wordIndex.begin(), wordIndex.end(), [](int &a) { return a == 0; });
-
-    return result;
+    return dp[s.size() - 1];
 }
 
 int main()
@@ -48,12 +48,8 @@ int main()
     vector<string> a2{"apple", "pen"};
     cout << wordBreak("applepenapple", a2) << endl;
 
-
     vector<string> a3{"cats", "dog", "sand", "and", "cat"};
     cout << wordBreak("catsandog", a3) << endl;
-
-    // vector<string> a4{"cats", "dog", "sand", "and", "cat"};
-    // cout << wordBreak("catsandog", a3) << endl;
 
     getchar();
     return 0;
