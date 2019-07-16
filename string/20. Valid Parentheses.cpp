b262b0dfc24c661fd120ea6ac7578fa146a10d48
DESKTOP-PRCLPLD\47613 <CharlesFeng207@gmail.com>
2019-07-16 20:22:35 +0800

    d

---
+++ string/20. Valid Parentheses.cpp
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/valid-parentheses/
+
+#include <algorithm>
+#include <iostream>
+#include <limits.h>
+#include <map>
+#include <numeric>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+bool isValid(string s)
+{
+}
+
+int main()
+{
+    cout << isValid("()[]{}") << endl;
+    cout << isValid("(]") << endl;
+    getchar();
+    return 0;
+}
