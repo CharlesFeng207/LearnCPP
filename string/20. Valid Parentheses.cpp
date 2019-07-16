009215238da56e5ce349c052aef288740a662dc7
DESKTOP-PRCLPLD\47613 <CharlesFeng207@gmail.com>
2019-07-16 20:33:47 +0800

    s

--- string/20. Valid Parentheses.cpp
+++ string/20. Valid Parentheses.cpp
@@ -14,11 +14,36 @@ using namespace std;
 
 bool isValid(string s)
 {
+    static map<char, bool> table{{'(', true}, {')', false}, {'[', true}, {']', false}, {'{', true}, {'}', false}};
+    stack<char> ps;
+
+    for (int i = 0; i < s.size(); i++)
+    {
+        auto iter = table.find(s[i]);
+
+        if (iter == table.end())
+            return false;
+
+        if (iter->second)
+        {
+            ps.push(iter->first);
+        }
+        else
+        {
+            if (ps.empty() || ps.top() != prev(iter)->first)
+            {
+                return false;
+            }
+            ps.pop();
+        }
+    }
+
+    return ps.empty();
 }
 
 int main()
 {
-    cout << isValid("()[]{}") << endl;
+    cout << isValid("()([]{()})") << endl;
     cout << isValid("(]") << endl;
     getchar();
     return 0;
